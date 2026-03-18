7da54e2900987e3431223e3242d08a5757e3b8d0
Aidan Diaz <diaz.aidan.d@gmail.com>
2026-03-18 10:31:14 -0400

    completed Reverse Linked List NeetCode

---
+++ linkedList/reverseLinkedList.js
@@ -0,0 +1,83 @@
+/* Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
+
+Example 1:
+
+Input: head = [0,1,2,3]
+
+Output: [3,2,1,0]
+Example 2:
+
+Input: head = []
+
+Output: []
+Constraints:
+
+0 <= The length of the list <= 1000.
+-1000 <= Node.val <= 1000 */
+
+//MY SOLUTION
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    reverseList(head) {
+
+        if(!head) {
+            return null
+        }
+
+        let newHead = head
+
+        if(head.next) {
+            newHead = this.reverseList(head.next)
+            head.next.next = head
+        }
+        head.next = null
+        return newHead
+        
+    }
+}
+
+
+//SOLUTION TO STUDY
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    reverseList(head) {
+        let prev = null;
+        let curr = head;
+
+        while (curr) {
+            let temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+}
