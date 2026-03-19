d06fe543234eff6364fdfcf1811d4c7da1da61db
Aidan Diaz <diaz.aidan.d@gmail.com>
2026-03-19 10:34:59 -0400

    completed Merge Two Sorted Linked Lists NeetCode

---
+++ linkedList/mergeTwoSortedLinkedLists.js
@@ -0,0 +1,106 @@
+/* You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted linked list and return the head of the new sorted linked list.
+
+The new list should be made up of nodes from list1 and list2.
+
+Example 1:
+
+
+
+Input: list1 = [1,2,4], list2 = [1,3,5]
+
+Output: [1,1,2,3,4,5]
+Example 2:
+
+Input: list1 = [], list2 = [1,2]
+
+Output: [1,2]
+Example 3:
+
+Input: list1 = [], list2 = []
+
+Output: [] */
+
+//MY SOLUTION
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} list1
+     * @param {ListNode} list2
+     * @return {ListNode}
+     */
+    mergeTwoLists(list1, list2) {
+
+        if(!list1) {
+            return list2
+        }
+
+        if(!list2) {
+            return list1
+        }
+
+        if(list1.val <= list2.val) {
+            list1.next = this.mergeTwoLists(list1.next, list2)
+            return list1
+        }else {
+            list2.next = this.mergeTwoLists(list1, list2.next)
+            return list2
+        }
+
+
+    }
+}
+
+//SOLUTION TO STUDY
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} list1
+     * @param {ListNode} list2
+     * @return {ListNode}
+     */
+    mergeTwoLists(list1, list2) {
+        const dummy = { val: 0, next: null };
+        let node = dummy;
+
+        while (list1 && list2) {
+            if (list1.val < list2.val) {
+                node.next = list1;
+                list1 = list1.next;
+            } else {
+                node.next = list2;
+                list2 = list2.next;
+            }
+            node = node.next;
+        }
+
+        if (list1) {
+            node.next = list1;
+        } else {
+            node.next = list2;
+        }
+
+        return dummy.next;
+    }
+}
