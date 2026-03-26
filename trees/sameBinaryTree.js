2fc1db8dbf3d8bbc356d81ad7c17a4b85c50d5cd
Aidan Diaz <diaz.aidan.d@gmail.com>
2026-03-25 23:53:18 -0400

    completed Same Binary Tree NeetCode

---
+++ trees/sameBinaryTree.js
@@ -0,0 +1,125 @@
+/* Given the roots of two binary trees p and q, return true if the trees are equivalent, otherwise return false.
+
+Two binary trees are considered equivalent if they share the exact same structure and the nodes have the same values.
+
+Example 1:
+
+
+
+Input: p = [1,2,3], q = [1,2,3]
+
+Output: true
+Example 2:
+
+
+
+Input: p = [4,7], q = [4,null,7]
+
+Output: false
+Example 3:
+
+
+
+Input: p = [1,2,3], q = [1,3,2]
+
+Output: false
+Constraints:
+
+0 <= The number of nodes in both trees <= 100.
+-100 <= Node.val <= 100Given the roots of two binary trees p and q, return true if the trees are equivalent, otherwise return false.
+
+Two binary trees are considered equivalent if they share the exact same structure and the nodes have the same values.
+
+Example 1:
+
+
+
+Input: p = [1,2,3], q = [1,2,3]
+
+Output: true
+Example 2:
+
+
+
+Input: p = [4,7], q = [4,null,7]
+
+Output: false
+Example 3:
+
+
+
+Input: p = [1,2,3], q = [1,3,2]
+
+Output: false
+Constraints:
+
+0 <= The number of nodes in both trees <= 100.
+-100 <= Node.val <= 100 */
+
+//MY SOLUTION
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {boolean}
+     */
+    isSameTree(p, q) {
+        if(!p && !q) return true
+
+        if(p && q && p.val === q.val) {
+            return this.isSameTree(p.left, q.left) && this.isSameTree(p.right, q.right)
+        }else {
+            return false
+        }
+    }
+}
+
+
+//SOLUTION TO STUDY
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {boolean}
+     */
+    isSameTree(p, q) {
+        const stack = [[p, q]];
+
+        while (stack.length) {
+            const [node1, node2] = stack.pop();
+
+            if (!node1 && !node2) continue;
+            if (!node1 || !node2 || node1.val !== node2.val) {
+                return false;
+            }
+            stack.push([node1.right, node2.right]);
+            stack.push([node1.left, node2.left]);
+        }
+
+        return true;
+    }
+}
