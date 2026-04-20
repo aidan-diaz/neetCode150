55020d7cb081f0fcfc52608a190bd478dd294cf9
Aidan Diaz <diaz.aidan.d@gmail.com>
2026-04-20 12:26:10 -0400

    completed old stack problems again to get a better understanding

---
+++ stackRefreshers/dailyTemperatures.js
@@ -0,0 +1,48 @@
+/* You are given an array of integers temperatures where temperatures[i] represents the daily temperatures on the ith day.
+
+Return an array result where result[i] is the number of days after the ith day before a warmer temperature appears on a future day. If there is no day in the future where a warmer temperature will appear for the ith day, set result[i] to 0 instead.
+
+Example 1:
+
+Input: temperatures = [30,38,30,36,35,40,28]
+
+Output: [1,4,1,2,1,0,0]
+Example 2:
+
+Input: temperatures = [22,21,20]
+
+Output: [0,0,0]
+Constraints:
+
+1 <= temperatures.length <= 1000.
+1 <= temperatures[i] <= 100 */
+
+//MY SOLUTION
+
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        const n = temperatures.length;
+        const res = new Array(n).fill(0);
+
+        for (let i = n - 2; i >= 0; i--) {
+            let j = i + 1;
+            while (j < n && temperatures[j] <= temperatures[i]) {
+                if (res[j] === 0) {
+                    j = n;
+                    break;
+                }
+                j += res[j];
+            }
+
+            if (j < n) {
+                res[i] = j - i;
+            }
+        }
+        return res;
+    }
+}
+
